efa99b9e1bec0d5e6dc7da4470ec0eb4762c21ef
LAPTOP-5SS0CNUL\Admin <hoangkhangtruong1705@gmail.com>
2025-05-23 10:42:04 +0700

    10th-commit

--- app/core/config.py
+++ app/core/config.py
@@ -31,7 +31,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     DOMAIN: str = "https://be-shopee-project.onrender.com"
-    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    ENVIRONMENT: Literal["local", "staging", "production"] = "production"
 
     @computed_field  # type: ignore[misc]
     @property
